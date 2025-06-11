37949583cab8091d1ac73b2821cc3061eb3f345d
andreapanico12 <andreapanico12@gmail.com>
2025-06-11 11:05:54 +0200

    remove unused links from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,6 @@ const Navbar = () => (
   <nav className="p-4">
     <Link to="/">Home</Link>
     <Link to="/favourites">Preferiti</Link>
-    <Link to="/details/1">Dettagli</Link>
-    <Link to="/not-found">Not Found</Link>
   </nav>
 );
 
