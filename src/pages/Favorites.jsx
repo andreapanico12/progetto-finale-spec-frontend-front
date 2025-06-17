6122f83ba98776c0d0007a37e1103cf185b24a59
andreapanico12 <andreapanico12@gmail.com>
2025-06-17 10:15:52 +0200

    implement Favorites page to display user's favorite bicycles using FavoritesContext and BicycleCard component

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useFavorites } from '../contexts/FavoritesContext';
+import BicycleCard from '../components/BicycleCard';
 
 function Favorites() {
+  const { favorites } = useFavorites();
+
   return (
-    <div>Favorites</div>
-  )
+    <div className="container mt-4">
+      <h1 className="mb-4">Le tue biciclette preferite</h1>
+
+      {favorites.length === 0 ? (
+        <p>Nessuna bicicletta nei preferiti.</p>
+      ) : (
+        <div className="row">
+          {favorites.map((bike) => (
+            <div key={bike.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+              <BicycleCard bike={bike} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Favorites
+export default Favorites;
